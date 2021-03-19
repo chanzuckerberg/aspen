f5097bda87caa9ac5e07d86f1abaf8503f179701
lvreynoso <lvreynoso@protonmail.com>
2021-03-19 13:17:07 -0700

    IT'S ALIVE

--- src/ts/config/paths.js
+++ src/ts/config/paths.js
@@ -53,7 +53,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp(".env"),
   appPath: resolveApp("."),
-  appBuild: resolveApp("../py/aspen/static"),
+  appBuild: resolveApp("dist"),
   appPublic: resolveApp("public"),
   appHtml: resolveApp("public/index.html"),
   appIndexJs: resolveModule(resolveApp, "src/index"),
