577b94f0d20a4ea3229ec5fa67e3329d8a98b95b
lvreynoso <lvreynoso@protonmail.com>
2021-04-09 10:54:59 -0700

    Plain auspice view with phylo tree id

--- src/py/aspen/app/views/auspice.py
+++ src/py/aspen/app/views/auspice.py
@@ -31,14 +31,23 @@ PHYLO_TREE_KEY = "phylo_trees"
 @application.route("/api/auspice/<int:phylo_tree_id>", methods=["GET"])
 @requires_auth
 def auspice(phylo_tree_id: int):
-    with session_scope(application.DATABASE_INTERFACE) as db_session:
-        profile = session["profile"]
-        user = (
-            get_usergroup_query(db_session, profile["user_id"])
-            .options(joinedload(User.group, Group.can_see))
-            .one()
-        )
+    # with session_scope(application.DATABASE_INTERFACE) as db_session:
+    #     profile = session["profile"]
+        # user = (
+        #     get_usergroup_query(db_session, profile["user_id"])
+        #     .options(joinedload(User.group, Group.can_see))
+        #     .one()
+        # )
 
+    view_string = url_for("auspice_view", _external=True, phylo_tree_id=phylo_tree_id)
+    view_string = view_string.replace("https://", "")
+
+    return redirect(f'https://nextstrain.org/fetch/{view_string}')
+
+@application.route("/api/auspice/view/<int:phylo_tree_id>/auspice.json", methods=["GET"])
+def auspice_view(phylo_tree_id: int):
+    phylo_tree: PhyloTree
+    with session_scope(application.DATABASE_INTERFACE) as db_session:
         phylo_runs: Iterable[Tuple[PhyloRun, int]] = (
             db_session.query(PhyloRun)
             .options(joinedload(PhyloRun.outputs))
@@ -51,7 +60,6 @@ def auspice(phylo_tree_id: int):
             .filter(PhyloRun.workflow_status == WorkflowStatusType.COMPLETED)
         )
 
-        phylo_tree: PhyloTree
         for phylo_run in phylo_runs:
             for output in phylo_run.outputs:
                 if isinstance(output, PhyloTree) and output.entity_id == phylo_tree_id:
@@ -59,28 +67,11 @@ def auspice(phylo_tree_id: int):
                     found = True
                     break
 
-        s3_client = boto3.client("s3")
+    s3_client = boto3.client("s3")
 
-        presigned_url = (
-            s3_client.generate_presigned_url(
-                "get_object",
-                Params={ "Bucket": phylo_tree.s3_bucket, "Key": phylo_tree.s3_key },
-                ExpiresIn=3600
-            )
-        )
-        logger.warn(presigned_url)
-
-        quoted_url = quote_plus(presigned_url)
-
-        view_string = url_for("auspice_view", _external=True, _scheme=os.getenv("PREFERRED_URL_SCHEME", "http"), presigned_url=quoted_url)
-        view_string = view_string.replace("https://", "")
-
-    return redirect(f'https://nextstrain.org/fetch/{view_string}')
-
-@application.route("/api/auspice/view/<string:presigned_url>/auspice.json", methods=["GET"])
-def auspice_view(presigned_url: str):
-    r = requests.get(unquote_plus(presigned_url))
-    response = make_response(r.content)
+    s3_response = s3.get_object(Bucket=phylo_tree.s3_bucket, Key=phylo_tree.s3_key)
+    f = s3_response.body
+    response = make_response(f)
     response.headers['Content-Type'] = 'text/json'
-    response.headers['Content-Disposition'] = 'attachment; filename=auspice.json'
+    response.headers['Content-Disposition'] = 'attachment; filename=example.json'
     return response
