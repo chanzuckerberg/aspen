d185c39e6078e8fff79aaec579033e90ce947dd8
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-04-30 17:45:27 -0700

    Only missing multi-catch-blocks now.

--- .happy/terraform/modules/gisaid-sfn/main.tf
+++ .happy/terraform/modules/gisaid-sfn/main.tf
@@ -41,18 +41,18 @@ locals {
   # Figure out how to wire up one state to the next
   next_keys = { for i, job in local.jobs: job["name"] => {
     for statename, state in local.states: statename => {
-        Key: lookup(state, "End", false) && try(length(job[i+1]["name"]), 0) == 0 ? "End" : "Next"
-        End: lookup(state, "End", false) && try(length(job[i+1]["name"]), 0) == 0 ? true : false
+        Key: lookup(state, "End", false) && try(length(local.jobs[i+1]["name"]), 0) == 0 ? "End" : "Next"
+        End: lookup(state, "End", false) && try(length(local.jobs[i+1]["name"]), 0) == 0 ? true : false
         Next: try(coalesce(
             lookup(state, "Next", "") == "HandleFailure" ? "HandleFailure" : null, # HandleFailure is always an end state
             length(lookup(state, "Next", "")) > 0 ? join("", [job["name"], state["Next"]]) : null, # If this had a Next parameter, forward it along to the next step in this job
-            lookup(state, "End", false) ? try(join("", [job[i+1]["name"], local.first_step]), null) : null # If this was an end state, send it to the next job FIXME this is hardcoded
+            lookup(state, "End", false) ? try(join("", [local.jobs[i+1]["name"], local.first_step]), null) : null # If this was an end state, send it to the next job FIXME this is hardcoded
           ), null)
-        CatchBlock: length(try(state["Catch"]["Next"], "")) == 0 ? {} : {
-          Catch: {
-            Next: contains(local.final_errors, state["Catch"]["Next"]) ? state["Catch"]["Next"] : join("", job["name"], state["Catch"]["Next"])
-        }}
-    } if contains(local.final_errors, statename)
+        CatchBlock: length(try(state["Catch"][0]["Next"], "")) == 0 ? {} : { # FIXME this assumes one catch block max per step
+          Catch: [ merge(state["Catch"][0], {
+            Next: contains(local.final_errors, state["Catch"][0]["Next"]) ? state["Catch"][0]["Next"] : join("", [job["name"], state["Catch"][0]["Next"]])
+        })]}
+    } if !contains(local.final_errors, statename)
   }}
 
   # Make a copy of our desired states
@@ -60,18 +60,19 @@ locals {
   # Make a copy for each job, but make the ends of each job point to the next.
   new_states = merge(flatten([ for job in local.jobs: [
     for statename, state in local.states: {
-    join("", [job["name"], statename]) = merge(state,
-      local.next_keys[job["name"]][statename]["CatchBlock"],
+    join("", [job["name"], statename]) = merge(
+      { for key, val in state: key => val if !contains(["Next", "End"], key) }, # All of the existing keys except End and Next
       {
         (local.next_keys[job["name"]][statename]["Key"]) = local.next_keys[job["name"]][statename]["Key"] == "End" ? local.next_keys[job["name"]][statename]["End"] : local.next_keys[job["name"]][statename]["Next"]
-      }
-    )} if contains(local.final_errors, statename)
+      },
+      local.next_keys[job["name"]][statename]["CatchBlock"], # Note, the ordering is important.
+    )} if !contains(local.final_errors, statename)
   ]])...)
   new_sfn = {
     "Comment" = local.sfn_def["Comment"]
-    "StartAt" = "Ingest${local.first_step}" # fixme
+    "StartAt" = "${local.jobs[0]["name"]}${local.first_step}"
     "TimeoutSeconds" = local.sfn_def["TimeoutSeconds"]
-    "States" = merge(local.new_states, {"HandleFailure": local.states["HandleFailure"]})
+    "States" = merge(local.new_states, {for err_state in local.final_errors: err_state => local.states[err_state]})
   }
 }
 resource "aws_sfn_state_machine" "state_machine" {
