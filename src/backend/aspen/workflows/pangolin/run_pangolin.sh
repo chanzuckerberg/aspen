40c0872df7d796c89fbe1b48de19a21cdce38f65
phoenixAja <phoenix@phoenixlogan.net>
2021-05-06 11:41:02 -0700

    pangolin

--- src/backend/aspen/workflows/pangolin/run_pangolin.sh
+++ src/backend/aspen/workflows/pangolin/run_pangolin.sh
@@ -3,12 +3,8 @@
 set -Eeuxo pipefail
 
 # install miniconda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init
-#source ~/.bashrc
 
 # install pangolin
 mkdir pangolin
